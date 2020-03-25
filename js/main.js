3cfa49b00cf70ef91378c57f7a32b63cad5bc182
sachithm <sachith.mendis@gmail.com>
2020-03-25 16:11:50 +0000

    homepage

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var i;
+for (i = 0; i < document.getElementsByClassName("page-link").length; i++) {
+  var colors = ["#ff0000", "#00ff00", "#0000ff"];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  document.getElementsByClassName("page-link")[i].style.color = random_color;
+}
